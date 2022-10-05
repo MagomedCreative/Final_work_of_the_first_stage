08a4e37569d611b00f0644296f01bf17cb543273
MagomedCreative <110766478+MagomedCreative@users.noreply.github.com>
2022-10-05 12:41:42 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿string[] array = { "project-d", "toyota", "86", "gr86" };
+string[] result = new string[array.Length];
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3) result[i] = array[i];
+    else result[i] = string.Empty;
+    if (result[i] != string.Empty) Console.Write($"{result[i]} ");
+}
